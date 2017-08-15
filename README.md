19b3768fafe0019a3687162e77d08b6ca56c01f6
Trevor Joynson (trevorj) <github@skywww.net>
2017-08-15 15:08:07 -0700

    Support Stylus, remove tampermonkey necesssity with it.

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 the-darkening
 =============
 
-Stylish theme and associated Tampermonkey script.
+Stylus/Stylish theme.
+
+* If using Stylish and not Stylus, a Tampermonkey script is included to tag iframes as iframes since there is no good
+  way to tell such things from CSS (wat).
+* With Stylus, the Tampermonkey script is *not* required, but if you want to install it anyway, I won't stop you. Go for
+  it, killer.
 
 The script is not required, but recommended. It's a tiny one liner used to realize if we're in an iframe or not.
 Without the script active, iframes will not be darkened.
 
 * Style (`theme.css`): https://userstyles.org/styles/137141/the-darkening
-* Script (`theme.js`): https://greasyfork.org/en/scripts/26202-the-darkening
+
+* When using Stylish, optional script to also blacken iframes: (`theme.js`): https://greasyfork.org/en/scripts/26202-the-darkening
+  Also, you may want to look into switching to the fork, it's quite better already and doesn't have the same politics getting
+  features into Stylish does.
 
 Well what does it look like?
 ----------------------------
--- theme.js
+++ theme.js
@@ -13,4 +13,69 @@
 
     // Apply CSS class name according to window frame status
     document.documentElement.className += (window.self == window.top ? " the-darkening-top" : " the-darkening-framed");
+
+    var increase_brightness = function(hex, percent){
+        // strip the leading # if it's there
+        hex = hex.replace(/^\s*#|\s*$/g, '');
+
+        // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+        if(hex.length == 3){
+            hex = hex.replace(/(.)/g, '$1$1');
+        }
+
+        var r = parseInt(hex.substr(0, 2), 16),
+            g = parseInt(hex.substr(2, 2), 16),
+            b = parseInt(hex.substr(4, 2), 16);
+
+        return '#' +
+        ((0|(1<<8) + r + (256 - r) * percent / 100).toString(16)).substr(1) +
+        ((0|(1<<8) + g + (256 - g) * percent / 100).toString(16)).substr(1) +
+        ((0|(1<<8) + b + (256 - b) * percent / 100).toString(16)).substr(1);
+    };
+
+    /**
+    * ('#000000', 50) --> #808080
+    * ('#EEEEEE', 25) --> #F2F2F2
+    * ('EEE     , 25) --> #F2F2F2
+    **/
+
+    function getImageLightness(imageSrc,callback) {
+        var img = document.createElement("img");
+        img.src = imageSrc;
+        img.style.display = "none";
+        document.body.appendChild(img);
+
+        var colorSum = 0;
+
+        img.onload = function() {
+            // create canvas
+            var canvas = document.createElement("canvas");
+            canvas.width = this.width;
+            canvas.height = this.height;
+
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(this,0,0);
+
+            var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+            var data = imageData.data;
+            var r,g,b,avg;
+
+            for(var x = 0, len = data.length; x < len; x+=4) {
+                r = data[x];
+                g = data[x+1];
+                b = data[x+2];
+
+                avg = Math.floor((r+g+b)/3);
+                colorSum += avg;
+            }
+
+            var brightness = Math.floor(colorSum / (this.width*this.height));
+            callback(brightness);
+        }
+    }
+
+    //getImageLightness("image.jpg",function(brightness){
+    //    console.log(brightness);
+    //});
+
 })();
